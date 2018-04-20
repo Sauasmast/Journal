63c6a8dfa122d4263876691b81fc091079b2e67d
Saugat  Dahal <saugatdahal@Saugats-MacBook-Pro.local>
2018-04-20 01:54:08 -0400

    All the views created

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,22 @@ router.post('/checking', function(req, res, next) {
   res.send("Check out 2 ----");
 });
 
+router.get('/ideas', function(req, res, next) {
+  var d = new Date();
+  var todaydate = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay();
+  res.render('./afterlogin/ideas', {date : todaydate});
+});
+
+router.get('/edit', function(req, res, next) {
+  var d = new Date();
+  var todaydate = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay();
+  res.render('./afterlogin/edit', {date : todaydate});
+});
+
+router.get('/lists', function(req, res, next) {
+  var d = new Date();
+  var todaydate = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay();
+  res.render('./afterlogin/list', {date : todaydate});
+});
+
 module.exports = router;
